a58a6e794ce13d381b7a5e9faa8bfc0647bc8593
Terrance-TT <yungyungadam@gmail.com>
2026-09-24 09:20:24 -0400

    assets: add img-club-fair aggregator

---
+++ src/assets/img-club-fair.ts
@@ -0,0 +1,13 @@
+// src-club-fair.png inlined as base64 parts so the repository stays text-only.
+import p1 from "./img-club-fair.p1";
+import p2 from "./img-club-fair.p2";
+import p3 from "./img-club-fair.p3";
+import p4 from "./img-club-fair.p4";
+
+const data = "data:image/jpeg;base64," +
+  p1 +
+  p2 +
+  p3 +
+  p4;
+
+export default data;
